d38d86fbc5aed38cbadae5b87392868b781edb69
Steve Hu <stevehu@gmail.com>
2018-06-16 17:38:12 -0400

    fixes #4 creating h2 database schemas when start test server

--- src/test/java/net/lightapi/tokenization/handler/TestServer.java
+++ src/test/java/net/lightapi/tokenization/handler/TestServer.java
@@ -2,13 +2,24 @@
 package net.lightapi.tokenization.handler;
 
 import com.networknt.server.Server;
+import com.networknt.service.SingletonServiceFactory;
+import org.h2.tools.RunScript;
 import org.junit.rules.ExternalResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import com.networknt.server.Server;
 import com.networknt.server.ServerConfig;
+
+
+import javax.sql.DataSource;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class TestServer extends ExternalResource {
     static final Logger logger = LoggerFactory.getLogger(TestServer.class);
@@ -23,6 +34,29 @@ public class TestServer extends ExternalResource {
     }
 
     private TestServer() {
+        DataSource ds = (DataSource) SingletonServiceFactory.getBean(DataSource.class);
+        try (Connection connection = ds.getConnection()) {
+            String schemaResourceName = "/scripts/create_tokenization.sql";
+            InputStream in = TestServer.class.getResourceAsStream(schemaResourceName);
+
+            if (in == null) {
+                throw new RuntimeException("Failed to load resource: " + schemaResourceName);
+            }
+            InputStreamReader reader = new InputStreamReader(in, UTF_8);
+            RunScript.execute(connection, reader);
+
+            schemaResourceName = "/scripts/create_vault.sql";
+            in = TestServer.class.getResourceAsStream(schemaResourceName);
+
+            if (in == null) {
+                throw new RuntimeException("Failed to load resource: " + schemaResourceName);
+            }
+            reader = new InputStreamReader(in, UTF_8);
+            RunScript.execute(connection, reader);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
